a4f75d636ec674cb61d3955da917bdfe3e43176a
brew-cristian <cplop@brewdigital.uk>
2025-02-05 11:44:52 +0000

    :zap: Sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://yourdomain.com',
+    siteUrl: 'https://doctor-chatbot-ai.vercel.app/',
     generateRobotsTxt: true,
     sitemapSize: 5000,
     exclude: ['/admin', '/dashboard'],
